0bb40efd372b81b32086c4b5d5f6df99823d7148
corey <corey.gao@mechanist.co>
2021-01-05 15:00:09 +0800

    添加Procedural Mesh, Vertex Shader Animation,  环境光照， 天空盒反射

---
+++ Assets/Scripts/GenMesh.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenMesh : MonoBehaviour
+{
+    public float MeshSize;
+    public int GridCount;
+
+    // Use this for initialization
+    void Start()
+    {
+        var mf = GetComponent<MeshFilter>();
+        BuildMeshData(MeshSize, GridCount);
+        mf.mesh = Gen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public Mesh Mesh { get; private set; }
+
+    private Vector3[] Vertices;
+    private int[] Triangles;
+    private Vector2[] UV;
+    private Vector2[] UV2;
+
+    private bool m_enableExtra;
+
+    private int GetNoiseIndex(int x)
+    {
+        return x * (m_enableExtra ? 2 : 1) + (m_enableExtra ? 2 : 1);
+    }
+
+    public void BuildMeshData(float meshSize, int gridCount, bool enableExtra = false)
+    {
+
+        int verticeCount = gridCount + 1;
+
+        if (verticeCount <= 0)
+            verticeCount = 1;
+
+        m_enableExtra = enableExtra;
+
+        int vn = !enableExtra ? (verticeCount * verticeCount) : (verticeCount * verticeCount + gridCount * gridCount);
+        Vertices = new Vector3[vn];
+        int triCoe = enableExtra ? 12 : 6;
+        Triangles = new int[verticeCount * verticeCount * triCoe];
+        UV = new Vector2[vn];
+        UV2 = new Vector2[vn];
+
+        float uu = 0, vv = 0;
+        int extraBegin = verticeCount * verticeCount;
+
+        for (int y = 0; y < verticeCount; y++)
+        {
+            vv = 0;
+            for (int x = 0; x < verticeCount; x++)
+            {
+                Vertices[y * verticeCount + x] = new Vector3(
+                    (x / (float)gridCount - 0.5f) * meshSize,
+                    0,
+                    (y / (float)gridCount - 0.5f) * meshSize);
+
+
+                UV[y * verticeCount + x] = new Vector2(uu, vv);
+                vv += 1f / verticeCount;
+                UV2[y * verticeCount + x] = new Vector2(x / (float)gridCount, y / (float)gridCount);
+
+                if (enableExtra)
+                {
+                    if (x < (verticeCount - 1) && y < (verticeCount - 1))
+                    {
+                        int extraIndex = extraBegin + y * (verticeCount - 1) + x;
+                        Vertices[extraIndex] = new Vector3(
+                            ((x + 0.5f) / (float)gridCount - 0.5f) * meshSize,
+                            0,
+                            ((y + 0.5f) / (float)gridCount - 0.5f) * meshSize
+                            );
+
+                        UV[extraIndex] = new Vector2(uu + 0.5f, vv - 0.5f);
+                        UV2[extraIndex] = new Vector2((x + 0.5f) / (float)verticeCount, (y + 0.5f) / (float)verticeCount);
+                    }
+                }
+            }
+            uu += 1f / verticeCount;
+        }
+
+        for (int i = 0; i < verticeCount - 1; i++)
+        {
+            for (int j = 0; j < verticeCount - 1; j++)
+            {
+                int lbIndex = i * verticeCount + j;
+                int rbIndex = i * verticeCount + j + 1;
+                int ltIndex = (i + 1) * verticeCount + j;
+                int rtIndex = (i + 1) * verticeCount + j + 1;
+                if (enableExtra)
+                {
+                    int midIndex = extraBegin + i * (verticeCount - 1) + j;
+
+                    Triangles[(i * verticeCount + j) * triCoe] = lbIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 1] = midIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 2] = rbIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 3] = rbIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 4] = midIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 5] = rtIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 6] = rtIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 7] = midIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 8] = ltIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 9] = ltIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 10] = midIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 11] = lbIndex;
+                }
+                else
+                {
+                    Triangles[(i * verticeCount + j) * triCoe] = lbIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 1] = ltIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 2] = rbIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 3] = rbIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 4] = ltIndex;
+                    Triangles[(i * verticeCount + j) * triCoe + 5] = rtIndex;
+                }
+            }
+        }
+    }
+
+    public Mesh Gen()
+    {
+        if (Mesh != null)
+            return Mesh;
+
+        Mesh m = new Mesh();
+        m.vertices = Vertices;
+        m.triangles = Triangles;
+        m.uv = UV;
+        m.uv2 = UV2;
+        Mesh = m;
+        /*
+        UnityEditor.AssetDatabase.CreateAsset(m, Application.dataPath + "/test.obj");
+        UnityEditor.AssetDatabase.SaveAssets();
+        */
+        return m;
+    }
+}
--- Assets/Shader/SimpleWater.shader
+++ Assets/Shader/SimpleWater.shader
@@ -6,12 +6,17 @@
         _EdgeColor("Edge Color", Color) = (1, 1, 1, 1)
         _EdgeWidth("EdgeWidth", Float) = 1.0
         _DepthRampTex("Depth Ramp Texture", 2D) = "white"{}
+        _NoiseTex("Noise Texture", 2D) = "white"{}
+        _WaveSpeed("Wave Speed", Float) = 1.0
+        _WaveAmp("Wave Amplifier", Float) = 1.0
+        _RelectionIntensity("Relection Intensity", Range(0, 1)) = 0.3
 
         [KeywordEnum(None, Simple, Ramp)] _Foam("Foam Mode", Float) = 0
+        [Toggle(ENABLE_ANIMATION)] _Anim("Vertex Animation?", Float) = 0
     }
     SubShader
     {
-        Tags { "RenderType"="Transparent" }
+        Tags { "RenderType"="Transparent" "LightMode"="ForwardBase"}
         Blend SrcAlpha OneMinusSrcAlpha
         Pass
         {
@@ -19,39 +24,73 @@
             #pragma vertex vert
             #pragma fragment frag
             #include "UnityCG.cginc"
+            #include "UnityLightingCommon.cginc"
 
+#pragma shader_feature ENABLE_ANIMATION
 #pragma multi_compile _FOAM_NONE _FOAM_SIMPLE _FOAM_RAMP
 
             struct appdata
             {
                 float4 vertex : POSITION;
+                float4 texCoord: TEXCOORD0;
             };
 
             struct v2f
             {
                 float4 vertex : SV_POSITION;
                 float4 screen_pos : TEXCOORD1;
+                float4 world_pos : TEXCOORD2;
             };
 
             sampler2D _CameraDepthTexture;
             sampler2D _DepthRampTex;
+            sampler2D _NoiseTex;
+
             float4 _Color;
+
             float4 _EdgeColor;
             float _EdgeWidth;
+
+            float _WaveSpeed;
+            float _WaveAmp;
+
+            float _RelectionIntensity;
 
             v2f vert (appdata v)
             {
                 v2f o;
+
+#if ENABLE_ANIMATION
+                float4 noise = tex2Dlod(_NoiseTex, float4(v.texCoord.xy, 0, 0));
+                float co = _Time * _WaveSpeed * noise;
+                v.vertex.x += sin(co) * _WaveAmp;
+                v.vertex.y += cos(co) * _WaveAmp;
+#endif
+
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 o.screen_pos = ComputeScreenPos(o.vertex);
-
+                o.world_pos = mul(unity_ObjectToWorld, v.vertex);
                 return o;
             }
 
             fixed4 frag(v2f i) : SV_Target
             {
+                float3 x_dif = ddx(i.world_pos).xyz;
+                float3 y_dif = ddy(i.world_pos).xyz;
+                float3 normal = -normalize(cross(x_dif, y_dif));
+
+                float3 worldViewDir = normalize(UnityWorldSpaceViewDir(i.world_pos.xyz));
+                float3 refL = reflect(-worldViewDir, normal);
+
+                half4 skyData = UNITY_SAMPLE_TEXCUBE(unity_SpecCube0, refL);
+                half3 _skyColor = DecodeHDR(skyData, unity_SpecCube0_HDR);
+                half4 skyColor = half4(_skyColor, 1);
+
+                float nl = saturate(dot(normal, _WorldSpaceLightPos0.xyz));
+                float3 diffuse = nl * _LightColor0;
+
 #if _FOAM_NONE
-                return _Color;
+                return _Color * nl + float4(ShadeSH9(half4(normal, 1)), 0) + skyColor * _RelectionIntensity;
 #else
                 float4 depth = SAMPLE_DEPTH_TEXTURE_PROJ(_CameraDepthTexture, i.screen_pos);
                 float viewZ = LinearEyeDepth(depth);
@@ -62,21 +101,8 @@
                 float4 foamRamp = tex2D(_DepthRampTex, float2(foamLine, 0.1));
                 float4 col = _Color * foamRamp;
 #endif
-                return col;
+                return col * nl + float4(ShadeSH9(half4(normal, 1)), 0) + skyColor * _RelectionIntensity;
 #endif
-                /*
-#if ENABLE_FOAM
-                float4 depth = SAMPLE_DEPTH_TEXTURE_PROJ(_CameraDepthTexture, i.screen_pos);
-                float viewZ = LinearEyeDepth(depth);
-                float foamLine =  saturate(_EdgeWidth * (viewZ - i.screen_pos.w));
-                //float4 col = _Color + (1 - foamLine )* _EdgeColor;
-                float4 foamRamp = tex2D(_DepthRampTex, float2(foamLine, 0.1));
-                float4 col = _Color + (1 - foamLine) * _EdgeColor;;
-
-                return col;
-#else
-#endif
-*/
             }
             ENDHLSL
         }
